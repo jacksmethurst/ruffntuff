42465a1cfa2b9b4008f93e5dc4caca0616345a94
Jack Smethurst <jack@jacksportfolio.com>
2022-06-15 11:37:14 +0100

    v

--- main-v3.js
+++
@@ -1,5 +0,0 @@
-// give body overflow-y: hidden;
-document.body.style.overflowY = 'hidden';
-
-// give ul.scroll-wrap width: max-content;
-document.querySelector('ul.scroll-wrap').style.width = 'max-content';
---
+++ main-v4.js
@@ -0,0 +1,8 @@
+// give body overflow-y: hidden;
+document.body.style.overflowY = 'hidden';
+
+// give ul.scroll-wrap width: max-content;
+document.querySelector('ul.scroll-wrap').style.width = 'max-content';
+
+// give ul.scroll-wrap overflow-x: scroll;
+document.querySelector('ul.scroll-wrap').style.overflowX = 'scroll';
